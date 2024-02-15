70278062aea490e8e23593a91b7ae6707d03d2dd
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-02-15 12:29:12 +0530

    college

---
+++ iftrinagle.java
@@ -0,0 +1,11 @@
+public class iftrinagle {
+    public static void main(String[] args) {
+        int s1 = 60, s2 = 60, s3 = 60;
+        int angle = s1 + s2 + s3;
+        if (angle == 180) {
+            System.out.println("Trinagle can be formed");
+        } else {
+            System.out.println("Triangle cannot be formed");
+        }
+    }
+}
