bb2ea9a0740c29a11abea55ba277f359c7298eb9
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-04-21 12:26:51 +0530

    formatted

--- switchStatement.java
+++ switchStatement.java
@@ -2,31 +2,41 @@ import javax.swing.JOptionPane;
 
 public class switchStatement {
     public static void main(String[] args) {
-        String s = JOptionPane.showInputDialog(null,"Enter a single digit no :");
+        String s = JOptionPane.showInputDialog(null, "Enter a single digit no :");
         int N = Integer.parseInt(s);
-        switch(N)
-        {
-            case 0: System.out.println("Zero");
+        switch (N) {
+            case 0:
+                System.out.println("Zero");
                 break;
-            case 1: System.out.println("One");
+            case 1:
+                System.out.println("One");
                 break;
-            case 2: System.out.println("Two");
+            case 2:
+                System.out.println("Two");
                 break;
-            case 3: System.out.println("Three");
+            case 3:
+                System.out.println("Three");
                 break;
-            case 4: System.out.println("Four");
+            case 4:
+                System.out.println("Four");
                 break;
-            case 5: System.out.println("Five");
+            case 5:
+                System.out.println("Five");
                 break;
-            case 6: System.out.println("Six");
+            case 6:
+                System.out.println("Six");
                 break;
-            case 7: System.out.println("Seven");
+            case 7:
+                System.out.println("Seven");
                 break;
-            case 8: System.out.println("Eight");
+            case 8:
+                System.out.println("Eight");
                 break;
-            case 9: System.out.println("Nine");
+            case 9:
+                System.out.println("Nine");
                 break;
-            default: System.out.println("Not a single digit number");
+            default:
+                System.out.println("Not a single digit number");
         }
     }
 }
