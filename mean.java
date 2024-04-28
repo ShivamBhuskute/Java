9e3e44bca640ce16d1b501c74baafd27fb64ad8a
ShivamBhuskute <bhuskuteshivam8@gmail.com>
2024-04-28 20:16:09 +0530

    syntax

--- mean.java
+++ mean.java
@@ -1,7 +1,7 @@
 public class mean {
     public static void main(String[] args) {
         int a = 5, b = 5, c = 5;
-        double mean  = (a+b+c)/3;
+        double mean = (a + b + c) / 3;
         System.out.println("Mean is " + mean);
     }
 }
--- nestedif.java
+++ nestedif.java
@@ -1,19 +1,15 @@
 public class nestedif {
     public static void main(String[] args) {
         int a = 50, b = 50, c = 50, d = 50, e = 50;
-        int total = a + b+ c + d + e;
+        int total = a + b + c + d + e;
         double per = total / 5;
-        if(a >= 40 && b >= 40 && c >= 40 && d >= 40 && e >= 40)
-        {
-            if(per > 65) {
+        if (a >= 40 && b >= 40 && c >= 40 && d >= 40 && e >= 40) {
+            if (per > 65) {
                 System.out.println("Distinction");
-            }
-            else{
+            } else {
                 System.out.println(per + "%");
             }
-        }
-        else
-        {
+        } else {
             System.out.println("Fail");
         }
     }
